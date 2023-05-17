2b0177342244277c9cc67119570fa4159fa60ccc
marco <marco.hudec3@gmail.com>
2023-05-17 14:37:41 +0200

    minus implementiert?

--- src/main/java/com/example/slmcalc/SlmCalcApplication.java
+++ src/main/java/com/example/slmcalc/SlmCalcApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SlmCalcApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SlmCalcApplication.class, args);
     }
 
